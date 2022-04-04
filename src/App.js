7cd2b3061fa76abcef05452a096a32e7bb29ef0e
Jack <jackowendickson@gmail.com>
2022-04-04 13:57:36 +0100

    arry is rendered

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import React, {useState} from 'react';
 function App() {
   const [tasks, setTasks] = useState([
     { taskName: "Walk the dog", isDone: false},
-    { taskName: "Wash Dishes", isDone: false}
+    { taskName: "Wash Dishes", isDone: false},
+    { taskName: "Feed Cat", isDone: true}
   ])
+
+  const taskNodes = tasks.map((task) => {
+    return (
+    <li className= {task.iDone ? 'completed':'not-completed'}>
+    <span>{task.taskName}</span>
+    </li>
+  )
+})
 
   return (
     <>
-      <h1>My Todo list</h1>
+      <h1>My ToDo list</h1>
+      <ul>
+        {taskNodes}
+      </ul>
     </>
   );
 }
